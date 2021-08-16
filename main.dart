31ed383823f5cfdb14f03c7a657443aa2c60a27d
Shrijan Prakash <64158567+shri05-stack@users.noreply.github.com>
2021-08-16 15:24:55 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:Shrijansrm264/home_page.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MOVIE APP',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
